35843645897227e9b828a95e2f81a725b4813598
Lucien Mollard <mollard.lucien@gmail.com>
2016-11-21 16:53:56 +0100

    modif models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :performance
   belongs_to :user
+  validates :start_date, presence: true, allow_blank: false
+  validates :end_date, presence: true, allow_blank: false
+
 end
