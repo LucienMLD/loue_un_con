801f1ae3dfcfaa531af6304ebbf87da956f9e46b
Lucien Mollard <mollard.lucien@gmail.com>
2016-11-21 17:26:35 +0100

    modify model performance

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -3,7 +3,6 @@ class Performance < ApplicationRecord
   has_many :bookings
   validates :speciality, presence: true, allow_blank: false
   validates :price, presence: true, allow_blank: false
-  validates :price, presence: true, allow_blank: false
   validates :area, presence: true, allow_blank: false
   validates :name, presence: true, allow_blank: false
 
