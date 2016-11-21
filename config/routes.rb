767fc53ebe37daebf2bc3258cc9301d1f5fd60ce
louis robert <dreyfus_rob@hotmail.com>
2016-11-21 19:17:47 +0100

    added log in and sign in forms + navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:show, :new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :performances do
-    resources :bookings, only: [:index, :show, :new, :create, :delete]
+    resources :bookings, only: [:new, :create, :index, :show, :delete]
   end
 end
