044c2a7a71cc8c43033a1ae7a0f673178e8306e8
louis robert <dreyfus_rob@hotmail.com>
2016-11-25 15:28:28 +0100

    improvement of accept button

--- app/assets/javascripts/accept_button.js
+++ app/assets/javascripts/accept_button.js
@@ -1,3 +1,5 @@
 $(".accept-button").on("click", function(event) {
   $(this).parent().addClass('hidden');
+  var accepted_message = $('<p>').html('<i class="fa fa-check-circle message-accepted-demand" aria-hidden="true"> demande acceptée </i>');
+  $(this).parent().parent().append(accepted_message);
 });
