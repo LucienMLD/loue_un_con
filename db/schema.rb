472c5f486e6993ab6fec39ea9b3ac245347f5472
louis robert <dreyfus_rob@hotmail.com>
2016-11-25 11:27:08 +0100

    db schema modified to resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161124163402) do
-
+ActiveRecord::Schema.define(version: 20161124183040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161124163402) do
     t.datetime "updated_at",     null: false
     t.integer  "duration"
     t.integer  "price"
+    t.boolean  "accepted"
     t.index ["performance_id"], name: "index_bookings_on_performance_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
@@ -86,10 +85,6 @@ ActiveRecord::Schema.define(version: 20161124163402) do
     t.datetime "token_expiry"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "full_address"
-    t.string   "zip_code"
-    t.string   "city"
-    t.string   "country"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
