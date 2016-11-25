db2196d3d1315b3fbcf202e9e5fa3014131dd133
louis robert <dreyfus_rob@hotmail.com>
2016-11-25 11:19:57 +0100

    dashboard jquery working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124142251) do
+ActiveRecord::Schema.define(version: 20161124183040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20161124142251) do
     t.datetime "updated_at",     null: false
     t.integer  "duration"
     t.integer  "price"
+    t.boolean  "accepted"
     t.index ["performance_id"], name: "index_bookings_on_performance_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
